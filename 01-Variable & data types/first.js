792d0aa27969fb4fcf780ebb802d5be9466c7c48
Subham67i <gitsubham67i@gmail.com>
2024-04-20 16:51:09 +0530

    Variable & data types

---
+++ 01-Variable & data types/first.js
@@ -0,0 +1,76 @@
+// ------------ Variable ------------
+
+
+// fullName = "subham";
+// age = 24;
+// price = 99.99;
+// x = null;
+// y = undefined;
+// isFollow = true;  
+// console.log(fullName);
+// isFollow = true;
+// console.log(isFollow);
+
+// -------------------- let --------------------
+
+// let fullName = "subham kumar sharma";
+// let age = 24;
+// let totalPrice = 100;
+// console.log(fullName);
+// {
+//     let a = 45;
+//     console.log(a);
+// }
+// {
+//     let b = 45;
+//     console.log(b);
+// }
+
+// ------------ var ---------------
+// var fullName = "subham kumar sharma";
+// var age = 24;
+// var totalPrice = 100;
+// console.log(fullName);
+
+// ------------- const --------------
+//  const PI = 3.14;
+
+// --------------------------------- data types -----------
+// primitive data types = number,string,boolean,null,undefine;
+// non-primitive data types = objects,(array, function);
+
+// object -
+// const student = {
+//     fullName : 'subham',
+//     age : 25,
+//     isPass : true
+// };
+// student['name'] = 'rahul'
+// console.log(student['name']);
+
+// QsNo.1 --- const object called product-
+
+// lets practice
+const Product = {
+    title : 'ball pen',
+    rating : 4,
+    price : 270,
+    offer : 5
+};
+console.log(Product);
+
+//QsNo.2-
+const Profile = {
+   name : 'shradhakhapra',
+   isFollow : true,
+   messge : 'hi',
+   posts : 196,
+   followers : 569,
+   following : 4,
+   bio : 'Apna Collage | Ex-Microsoft, DRDO'
+}
+console.log(Profile);
+
+
+
+
