a526b1db69996bab4724d0aab090a1f4f46cc2a5
Subham67i <gitsubham67i@gmail.com>
2024-04-22 23:58:44 +0530

    operator-js

---
+++ 02-Operator-JS/app.js
@@ -0,0 +1,107 @@
+//  Artimetic Operators 
+   
+// let a = 10;
+// let b = 7;
+// let c = a + b
+// console.log('a =', a, '& b =', b);
+// console.log('a + b =', a + b);
+// // console.log(c);
+// console.log('a - b =', a - b);
+// console.log('a * b =', a * b);
+// console.log('a / b =', a / b); 
+// console.log('a % b =', a % b);
+// // Expo 
+// console.log('a ** b =', a ** b);
+
+// Unary operators (increment ++), (decrement --);
+// ---------- (increment ++) ------------- ;
+// let a = 7;
+// let b = 8;
+// console.log('a =', a , '& b =',b);
+// // a = a + 1
+// // a++
+// console.log('a++ = ', a++); //7
+// console.log(a); //8
+
+// // ---------(decrement --) ---------;
+// let x = 5;
+// let y = 2;
+// console.log('x =', x , '& =', y);
+// // x = x - 1
+// // x--
+// console.log('x-- =' , x--);
+// console.log(x); 
+
+// ------------------------- Assignment operators -------------------;
+//  let a = 6;
+//  let b = 5 ;
+// //  a += 5
+//  a = a + 5
+//  console.log('a = ', a); // 11
+
+//  let x = 6;
+//  let y = 5 ;
+// //  a *= 5
+//  x = x * 5
+//  console.log('x = ', x); // 30
+
+
+// -------- Comparison Operators ---------------
+//  let a = 5;
+//  let b = 10;
+//  console.log('a == b' , a == b);  // false
+
+//  let x = 8;
+//  let y = 6;
+//  let z = x != y;
+//  console.log('x != y',z); // true
+
+//  let a = 5;
+//  let b = '10';
+//  let c = a === b;
+//  console.log('a === b ',c); //false
+
+//  let x = 7;
+//  let y = 6;
+//  let z = x !== y;
+//  console.log('x !== y',z); // true
+
+// let x = 7;
+// let y = 6;
+// let z = x > y;
+// console.log('x > y',z) // true
+
+// let x = 7;
+// let y = 6;
+// let z = x <= y;
+// console.log('x < y',z) // false
+
+// -------------- Logical Operators -------------
+
+// let x = 7;
+// let y = 6;
+// let cond1 = x > y; // true
+// let cond2 = x === 7; // true
+// console.log('cond1 && cond2', cond1 && cond2);  // true
+
+let x = 7;
+let y = 6;
+let cond1 = x < y; // true
+let cond2 = x === 7; // true
+console.log('cond1 && cond2', cond1 && cond2); // false
+
+// let a = 10;
+// let b = 30;
+// console.log('cond1 || cond2', a < b || b === 20); // true
+
+let a = 20;
+let b = 30;
+console.log('!(a > b)', a > b); // false
+
+
+
+
+
+
+ 
+
