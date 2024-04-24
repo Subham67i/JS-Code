1830fd014093342edd4a334737d2ff8ca66fc9d0
Subham67i <gitsubham67i@gmail.com>
2024-04-24 22:40:02 +0530

    Condational-Statements

---
+++ 03-Condational-Statements/app.js
@@ -0,0 +1,132 @@
+// conditional statements
+
+// if statement ----
+// let age = 5;
+
+// if(age >= 18){
+//     console.log('you can vote');
+// }
+// if(age < 18){
+//     console.log('you cannot vote');
+// }
+ 
+// let mode = 'light';
+// let color;
+// if(mode === 'dark'){
+//     color = 'black';
+// }
+// if(mode === 'light'){
+//     color = 'white'
+// }
+// console.log(color); // white
+
+// ------------- if-else condation
+
+// let mode = 'dark';
+// let color;
+
+// if(mode === 'dark'){
+//     color = 'black';
+// }else{
+//     color = 'white';
+// }
+// console.log(color);
+//------------------------
+// let age = 20;
+//  if(age >= 18){
+//     console.log('vote');
+//  }else{
+//     console.log('not vote');
+//  }
+ //----------------------------
+ // 123 odd , 246 even
+//  let num = 22;
+//  if(num % 2 === 0){
+//     console.log(num, 'is even');
+//  }else{
+//     console.log(num, 'is odd');
+//  }
+
+ // ----------- else-if statement
+
+//  let age = 25;
+//  if(age < 18){
+//     console.log( age,'junier');
+//  }else if(age >= 60){
+//     console.log(age,'siner');
+//  }else{
+//     console.log(age,'middle');
+//  }
+ //---------------------
+ 
+//  let moode = 'pink';
+//  let colors;
+
+//  if(moode === 'dark'){
+//     colors = 'black';
+//  }else if(moode === 'pink'){
+//     colors = 'pink';
+//  }else{
+//     colors = 'white'
+//  }
+//  console.log(colors);
+
+// ------------ ternary operators  -------------  
+
+// let age = 18;
+// let result = age >= 18 ?  'adult' : 'not adult';
+// console.log(result);
+
+//  ---- prompt ----
+// let name1 = prompt('hello');
+// console.log(name1); // subham
+
+// ------------- if statement ----
+let mode = "light";
+let color;
+
+if(mode === "dark"){
+    color = 'black';
+
+}if(mode === 'light'){
+    color = 'white'
+}
+console.log(color);
+
+// if-else  statement ---
+let num = 2;
+let resu;
+if(num % 2 === 0){
+    console.log('even  num ');
+}else{
+    console.log('odd  num ');
+}
+
+
+ // else-if statement ----
+//  let markes = prompt('enter an number 0 to 100');
+//  let grad;
+//  if(markes >= 90 && markes <= 100 ){
+//     grad = 'A+';
+//  }else if(markes <= 89 && markes >= 80){
+//     grad = 'A';
+//  }else if(markes <= 79 && markes >= 70){
+//     grad = 'B+';
+//  }else if(markes <= 69 && markes >= 60 ){
+//     grad = 'B';
+//  }else if(markes <= 59 && markes >= 50){
+//     grad = 'C+';
+//  }else if(markes <= 49 && markes >= 40){
+//     grad = "C";
+//  }else if(markes <= 39 && markes >= 0 ){
+//     grad = 'try again pass the exam  ........'
+//  }else{
+//     grad = 'inter a valid number'
+//  }
+//  console.log(grad);
+
+ // ---  ternary operater ---- 
+let age = 18;
+let result = age >= 18 ? 'adult' : 'not adult';
+console.log(result);
+
