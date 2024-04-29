b008eb20729027dd74c3c155e49cb8e428bc68d0
Subham67i <gitsubham67i@gmail.com>
2024-04-29 16:03:23 +0530

    string in js

---
+++ 06-String-in-JS/app.js
@@ -0,0 +1,42 @@
+//  -------------- string ------------ 
+
+// let str = 'subham';
+// console.log(str.length); // length str 
+
+// console.log(str[0],str[1],str[5]); // indexnumber
+
+
+// ---------- template literals -----------
+
+// let specialString = `this is a template literals`;
+// console.log(specialString);
+
+// example --------
+// let obj = {
+//     item : 'pen',
+//     price : 20
+// };
+// let output = `ths cost of ${obj.item} is ${obj.price} rupies`;
+// console.log(output);
+// console.log('the cost of',obj.item,'is',obj.price,'rupies');
+
+// -------------- string methods ------------------
+// let name = 'subham kumar';
+// let newName = name.toUpperCase();
+// console.log(name);
+// console.log(newName);
+
+// let name1 = 'subham kumar';
+// console.log(name1.toLowerCase());
+
+// lets practice -----------
+
+//Q.no-1 :-
+// prompt the user to enter full name . generate a username for the them based on the input.
+// start usernmae with @ followed by their full name and ending with fullname length.
+
+// ex - user name = "subham" , username should be "@subham6"
+
+// let fullName = prompt('enter your fullname without space');
+// let userName = "@" + fullName + fullName.length;
+// console.log(userName);
